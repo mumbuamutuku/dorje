7036942105826dd91aa671eda25ea37fad320b4a
mumbuamutuku <104358257+mumbuamutuku@users.noreply.github.com>
2023-05-22 09:24:48 +0000

    Requirements.txt

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ JOBS = [
 def hello_world():
     return render_template('home.html', jobs=JOBS, company_name='Dorje')
 
-@app.route("api/jobs")
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
   
